c264a2364979a14c59d424847b121b2e04c1bb91
agung343 <agungwahydi343@gmail.com>
2023-09-08 11:16:43 +0700

    needs to be reviewed

--- src/middleware/isAuth.ts
+++ src/middleware/isAuth.ts
@@ -26,31 +26,3 @@ export async function isAuth(req: Request, res: Response, next: NextFunction) {
         return next(new CustomError("An Internal Server Error!", 500))
     }
 }
-
-// export function isAuth(req: Request, res: Response, next: NextFunction) {
-//     const authHeader = req.get("Authorization")
-//     if (!authHeader) {
-//         return next(new CustomError("Not Authenticated", 401))
-//     }
-//     const token = authHeader?.split(" ")[1] as string
-//     let decodedToken;
-//     try {
-//         decodedToken = jwt.verify(token, "supersecretblog")
-//     } catch (error) {
-//         if (error instanceof jwt.JsonWebTokenError) {
-//             return next(new CustomError("Invalid Token", 401))
-//         } else if (error instanceof jwt.TokenExpiredError) {
-//             return next(new CustomError("Token Expired", 401))
-//         } else {
-//             return next(new CustomError("An Internal Server Error!", 500))
-//         }
-//     }
-
-//     if (!decodedToken) {
-//         return next(new CustomError("Not Authenticated", 401))
-//     }
-
-//     req.userId = decodedToken.userId;
-
-//     next()
-// }
